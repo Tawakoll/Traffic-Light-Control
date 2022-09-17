95bba8a649d9edac0b56adfd94158381e0ddeea2
Tawakoll <mohamedtawakol12@gmail.com>
2022-09-17 16:17:50 +0200

    remote c

--- TrafficLightsProject/TrafficLightsProject/Microcontroller Abstraction Layer/Timer/timer.c
+++ TrafficLightsProject/TrafficLightsProject/Microcontroller Abstraction Layer/Timer/timer.c
@@ -4,3 +4,12 @@
  * Created: 14/09/2022 11:40:28 PM
  *  Author: main user
  */ 
+
+#include "timer.h"
+volatile void (*CallBackPtr1) (void)= NULL;
+
+ISR(TIMER0_COMP_vect)
+{
+	
+}
+ISR(TIMER0_OVF_vect){}
--- TrafficLightsProject/TrafficLightsProject/Microcontroller Abstraction Layer/Timer/timer.h
+++ TrafficLightsProject/TrafficLightsProject/Microcontroller Abstraction Layer/Timer/timer.h
@@ -1,16 +1,37 @@
 /*
- * timer.h
- *
- * Created: 14/09/2022 11:40:40 PM
- *  Author: main user
- */ 
+* timer.h
+*
+* Created: 14/09/2022 11:40:40 PM
+*  Author: main user
+*/
 
 
 #ifndef TIMER_H_
 #define TIMER_H_
+#define NULL ((void*) 0)
+typedef enum en_timer_t {timer0,timer1,timer2}en_timer_t;
+typedef enum en_timerMode_t {normal,CTC}en_timerMode_t;
+typedef enum en_timerInterrupt_t {ENABLE,DISABLE}en_timerInterrupt_t;
+typedef enum en_timerPrescaler_t {N_0,N_1,N_8,N_64,N_256,N_1024}en_timerPrescaler_t;
 
+typedef struct st_timerConfig_t{
+	uint16_t timerInitialValue;
+	uint16_t comparelValue;
+	en_timer_t selectedTimer;
+	en_timerMode_t mode;
+	en_timerInterrupt_t timerInterrupt;
 
+	
+	
+}st_timerConfig_t;
 
+TIMER_init(st_timerConfig_t * configStruct);
+TIMER_reset(en_timer_t selectedTimer);
+TIMER_setCallback (void (*functionPtr) (void) ) ;
 
+ISR(TIMER0_COMP_vect){
+	
+}
+ISR(TIMER0_OVF_vect){}
 
 #endif /* TIMER_H_ */
